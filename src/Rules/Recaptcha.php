00315a7193348b9699591d04fb386fe26dafee61
apdev <alberto.piccolo.developer@gmail.com>
2021-11-26 12:51:25 +0100

    POST recaptcha

--- src/Rules/Recaptcha.php
+++ src/Rules/Recaptcha.php
@@ -5,25 +5,33 @@ use LawnMower\Rule;
 
 class Recaptcha extends Rule {
 
-    protected $error_message = "Invalid Recaptcha.";
+    protected $verify_url = "https://www.google.com/recaptcha/api/siteverify";
 
     public function isValid():bool {
+
         if(empty($this->params)){
-			throw new \InvalidArgumentException("Missing params");
-		}
-
-        $secret = $this->params[0];
-
-        if(empty($this->value)){
-            return false;
+            throw new \InvalidArgumentException("Missing params");
         }
 
-        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$this->value);
-        $responseData = json_decode($verifyResponse);
+        $data = [
+            'secret' => $this->params[0],
+            'response' => $this->value,
+        ];
+
+        $curl = curl_init($this->verify_url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        $responseData = json_decode($response);
 
         if($responseData->success){
             return true;
         }
+
+        $this->error_message = "Recaptcha Error: " . implode(", ",$responseData->{'error-codes'});
 
         return false;
     }
